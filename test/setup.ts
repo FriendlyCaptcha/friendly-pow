c9e377e3c4593aaa7d07bee984f353703ddf666b
Guido Zuidhof <me@guido.io>
2020-06-29 04:11:47 +0100

    Fix test env

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 const globalScope = typeof window !== "undefined" && window || typeof global !== "undefined" && global || self;
 Object.assign(globalScope, {
-    ASC_VERSION: 0,
+    ASC_TARGET: 0,
     // unchecked: (v: any) => v,
 });
