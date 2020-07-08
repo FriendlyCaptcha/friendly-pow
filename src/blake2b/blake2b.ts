752ed57bbeb79b0c17c291411bb0edda4e4a9685
Guido Zuidhof <me@guido.io>
2020-07-08 15:39:55 +0100

    Prevent use of TypedArray.map for old browser compatability

--- src/blake2b/blake2b.ts
+++ src/blake2b/blake2b.ts
@@ -121,14 +121,13 @@ export function B2B_G(v: Uint32Array, m: Uint32Array, a: u32, b: u32, c: u32, d:
 }
 
 // Initialization Vector
-const BLAKE2B_IV32 = new Uint32Array(16).map(
-  (v, i) =>
-    [
+const BLAKE2B_IV32: u32[] = [
       0xF3BCC908, 0x6A09E667, 0x84CAA73B, 0xBB67AE85,
       0xFE94F82B, 0x3C6EF372, 0x5F1D36F1, 0xA54FF53A,
       0xADE682D1, 0x510E527F, 0x2B3E6C1F, 0x9B05688C,
       0xFB41BD6B, 0x1F83D9AB, 0x137E2179, 0x5BE0CD19
-    ][i]);
+];
+
 
 
 // TODO format more nicely
