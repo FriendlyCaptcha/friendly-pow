8e276bf5e035c1d6f5905f804d0aa21b758b980d
MaxGraey <maxgraey@gmail.com>
2020-07-17 11:12:48 +0300

    minor refactorings

--- src/encoding.ts
+++ src/encoding.ts
@@ -2,7 +2,7 @@
  * Maps a value between 0 and 255 to a difficulty threshold (as uint32)
  * Difficulty 0 maps to 99.99% probability of being right on the first attempt
  * Anything above 250 needs 2^32 tries on average to solve.
- * 150 to 180 seems reasonable 
+ * 150 to 180 seems reasonable
  */
 export function difficultyToThreshold(value: u8): u32 {
     if (value > 255) {
@@ -11,7 +11,7 @@ export function difficultyToThreshold(value: u8): u32 {
         value = 0;
     }
 
-    return (Math.pow(2, (255.999 - (value as f64))/8.0) as u32 >>> 0);
+    return (Math.pow(2, (255.999 - (value as f64)) / 8.0) as u32 >>> 0);
 }
 
 /**
--- src/solverWasm.ts
+++ src/solverWasm.ts
@@ -11,16 +11,16 @@ const HASH_SIZE_BYTES = 32;
 
 /**
  * Solve the blake2b hashing problem, re-using the memory between different attempts (which solves up to 50% faster).
- * 
+ *
  * This only changes the last 4 bytes of the input array to find a solution. To find multiple solutions
  * one could call this function multiple times with the 4 bytes in front of those last 4 bytes varying.
- * 
- * 
+ *
+ *
  * The goal is to find a nonce that, hashed together with the rest of the input header, has a value of its
  * most significant 32bits that is below some threshold.
  * Approximately this means: the hash value of it starts with K zeroes (little endian), which is expected to be
  * increasingly difficult as K increases.
- * 
+ *
  * In practice you should ask the client to solve multiple (easier) puzzles which should reduce variance and also allows us
  * to show a progress bar.
  * @param input challenge bytes
@@ -28,7 +28,7 @@ const HASH_SIZE_BYTES = 32;
  */
 export function solveBlake2bEfficient(input: Uint8Array, threshold: u32, n: u32): Uint8Array {
     if (input.length != CHALLENGE_SIZE_BYTES) {
-        throw Error("Invalid input");
+        throw new Error("Invalid input");
     }
 
     const arrayPtr = changetype<usize>(input);
@@ -36,10 +36,10 @@ export function solveBlake2bEfficient(input: Uint8Array, threshold: u32, n: u32)
 
     const ctx = new Context(HASH_SIZE_BYTES);
     ctx.t = CHALLENGE_SIZE_BYTES;
-    
+
     const start = load<u32>(bufferPtr + 124);
     const end = start + n;
-    for(let i: u32 = start; i < end; i++) {
+    for (let i: u32 = start; i < end; i++) {
         store<u32>(bufferPtr + 124, i);
         // log(input.slice(124));
 
